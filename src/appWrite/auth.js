c104b92d4e0dd1561331959b6b99e3e676f2925e
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2023-11-20 19:42:38 +0530

    Added auth services

---
+++ src/appWrite/auth.js
@@ -0,0 +1,61 @@
+import conf from "../conf/config.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+   client = new Client();
+   account;
+
+   constructor(){
+      this.client
+         .setEndpoint(conf.appWriteUrl)
+         .setProject(conf.appWriteProjectID);
+      this.account = new Account(this.client);
+   }
+
+   async  createAccount({email, password, name}){
+      try{
+         const userAccount = await this.account.create(ID.unique(), email, password, name);
+         if(userAccount){
+            // call method to redirect
+            return this.loginAccount({email, password})
+         }else{
+            return userAccount
+         }
+      }catch(error){
+         console.error("Error:- createAccount method");
+         throw error;
+      }
+   }
+
+   async loginAccount({email, password}){
+      try{
+         return await this.account.createEmailSession(email, password);
+      }catch(error){
+         console.error("Error:- loginAccount method");
+         throw error
+      }
+   }
+   async logoutAccount(){
+      try {
+         await this.account.deleteSessions();         
+      } catch (error) {
+         console.error("Error:- logoutAccount method");
+         throw error;
+      }
+   }
+
+   async getCurrentUser(){
+      try {
+         return await this.account.get();
+      } catch (error) {
+         console.error("Error:- getCurrentUser method");
+         throw error;
+      }
+
+      return null;
+   }
+}
+
+const authService = new AuthService;
+
+export default authService;
--- src/conf/config.js
+++ src/conf/config.js
@@ -1,9 +1,9 @@
 const conf = {
    appWriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
-   appWriteUrl:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-   appWriteUrl:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-   appWriteUrl:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-   appWriteUrl:String(import.meta.env.VITE_APPWRITE_BUCKECT_ID),
+   appWriteProjectID:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+   appWriteDatabaseID:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+   appWriteCollectionID:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+   appWriteBucketID:String(import.meta.env.VITE_APPWRITE_BUCKECT_ID),
 }
 
 

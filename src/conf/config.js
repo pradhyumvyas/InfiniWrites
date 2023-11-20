fd0015ffc647f9fe4e7ff6627fae67b8024aef8d
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2023-11-20 18:55:10 +0530

    Project setup

---
+++ src/conf/config.js
@@ -0,0 +1,10 @@
+const conf = {
+   appWriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
+   appWriteUrl:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+   appWriteUrl:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+   appWriteUrl:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+   appWriteUrl:String(import.meta.env.VITE_APPWRITE_BUCKECT_ID),
+}
+
+
+export default conf

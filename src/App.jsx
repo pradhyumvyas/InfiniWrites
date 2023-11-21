dd575f6c9e6337431e5c2f5c31d404b8150b4634
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2023-11-21 13:42:29 +0530

    Configure redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from "./appWrite/auth"
+import {login, logout} from "./store/authSlice"
+import {Header, Footer} from "./components/index.js";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+      .then((data)=>{
+        if(data){
+          dispatch(login({data}))
+        }else{
+          dispatch(logout())
+        }
+      })
+      .finally(()=>{
+        setLoading(false);
+      })
+  },[])
   
-  return (
-    <>
-      <div>My Blog App</div>
-    </>
-  )
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+        <div className="w-full block">
+          <Header />
+          <main>
+            TODO:{/* <Outlet /> */}
+          </main>
+          <Footer />
+        </div>
+    </div>
+  ): null
 }
 
 export default App
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Header from "./header/Header.jsx"
+import Footer from "./footer/Footer.jsx"
+
+export{
+   Header, Footer
+}

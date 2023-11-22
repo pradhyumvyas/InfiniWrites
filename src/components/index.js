abfe11db30a6ecece79fb2440db13f68ef32cae6
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2023-11-22 15:22:24 +0530

    Implement RTE and react-hook-form

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ import Footer from "./footer/Footer.jsx"
 import Container from "./container/Container.jsx"
 import Logo from "./Logo.jsx"
 import LogoutBtn from "./header/LogoutBtn.jsx"
+import RTE from "../components/RTE.jsx"
 export{
-   Header, Footer, Container, Logo, LogoutBtn
+   Header, Footer, Container, Logo, LogoutBtn, RTE
 }
